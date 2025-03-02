93070bc6be4fdb81954670ebe60c3169b1731caf
RubaaAlghamdi <ralghamdi1255@stu.kau.edu.sa>
2025-03-02 22:44:07 +0300

    transction class for release 1

--- src/cpit251_project/Transaction.java
+++ src/cpit251_project/Transaction.java
@@ -1,7 +1,42 @@
 
 package cpit251_project;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
 
 public class Transaction {
     
+   
+    private String  TransactionName;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Transaction(String TransactionName, LocalDate startDate, LocalDate endDate) {
+        this.TransactionName = TransactionName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    
+
+    public String getDescription() {
+        return TransactionName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction Name: " + TransactionName +
+               "\nStart Date: " + startDate +
+               "\nEnd Date: " + endDate;
+    
+}
 }
