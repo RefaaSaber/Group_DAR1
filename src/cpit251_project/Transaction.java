1770fbf8a98bd116a1330aea6d4ae2e85d0a3ac9
RubaaAlghamdi <ralghamdi1255@stu.kau.edu.sa>
2025-03-27 20:25:04 +0300

    I modified the ID generator to be unique and modified the Get and toString to be compatible

--- src/cpit251_project/Transaction.java
+++ src/cpit251_project/Transaction.java
@@ -1,23 +1,23 @@
 package cpit251_project;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public class Transaction {
 
     private String transactionName;
     private LocalDate startDate;
     private LocalDate endDate;
-    private static int idCounter = 1; 
-    private int ID;
+    private String ID; 
 
     public Transaction(String TransactionName, LocalDate startDate, LocalDate endDate) {
         this.transactionName = TransactionName;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.ID = idCounter++;
+        this.ID = UUID.randomUUID().toString(); 
     }
     
-    public int getID(){
+    public String getID() {
         return ID;
     }
 
@@ -48,7 +48,7 @@ public class Transaction {
     
     @Override
     public String toString() {
-         return String.format("Transaction ID: %d%nTransaction Name: %s%nStart Date: %s%nEnd Date: %s",
+        return String.format("Transaction ID: %s%nTransaction Name: %s%nStart Date: %s%nEnd Date: %s",
                 ID, transactionName, startDate, endDate);
     }
 }
