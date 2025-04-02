3850a79b138bfe51405e34bd1fcdc453a88bf452
RefaaSaber <rhusainsaber@stu.kau.edu.sa>
2025-04-02 20:30:03 +0300

    Relase3

--- src/cpit251_project/EmployeeAssignment.java
+++ src/cpit251_project/EmployeeAssignment.java
@@ -3,4 +3,18 @@ package cpit251_project;
 
 public class EmployeeAssignment {
     
+     private String employeeName;
+    private LocalDate deadline;
+
+    public EmployeeAssignment(String employeeName, LocalDate deadline) {
+        this.employeeName = employeeName;
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Employee: %s, Deadline: %s", employeeName, deadline);
+    }
+}
+    
 }
