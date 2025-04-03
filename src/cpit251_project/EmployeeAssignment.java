0c588e72225facd9aa92a84e2d8aa562402fab6f
RemasAlahmari <ralahmari0031@stu.kau.edu.sa>
2025-04-03 18:54:15 +0300

    Refactoring for the EmployeeAssignment - Added getter

--- src/cpit251_project/EmployeeAssignment.java
+++ src/cpit251_project/EmployeeAssignment.java
@@ -3,12 +3,21 @@ package cpit251_project;
 import java.time.LocalDate;
 
 public class EmployeeAssignment {
+
     private String employeeName;
     private LocalDate deadline;
 
     public EmployeeAssignment(String employeeName, LocalDate deadline) {
         this.employeeName = employeeName;
         this.deadline = deadline;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
     }
 
     @Override
