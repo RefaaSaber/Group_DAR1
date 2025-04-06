a78bf84095e54280f9549ccc6aa134a62c413dfa
RemasAlahmari <ralahmari0031@stu.kau.edu.sa>
2025-04-06 20:01:14 +0300

    Edited the Employee Class

--- src/cpit251_project/Employee.java
+++ src/cpit251_project/Employee.java
@@ -1,7 +1,16 @@
-
 package cpit251_project;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Employee {
-    
+
+    private String name;
+    private List<Transaction> assignedTransactions;
+
+    public Employee(String name) {
+        this.name = name;
+        this.assignedTransactions = new ArrayList<>();
+    }
+
 }
