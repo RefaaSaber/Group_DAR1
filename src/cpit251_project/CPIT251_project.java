867a579767e30233baabc7e1a358f24871aef66b
LayanAbdullahAlghamdi <lalghamdi0180@stu.kau.edu.sa>
2025-04-07 23:24:38 +0300

    Updating main

--- src/cpit251_project/CPIT251_project.java
+++ src/cpit251_project/CPIT251_project.java
@@ -7,55 +7,38 @@ import java.util.Scanner;
 public class CPIT251_project {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
         Manager manager = new Manager();
-
         List<Transaction> allTransactions = new ArrayList<>();
 
         while (true) {
-
             System.out.println("Are you a (1) Manager or (2) Employee? (3 to Exit)");
-
             String role = scanner.nextLine().trim();
 
             if (role.equals("1")) {
-
                 Transaction transaction = manager.createTransaction();
 
                 if (transaction != null) {
-
                     allTransactions.add(transaction);
-
                     TransactionFileClass transactionFile = new TransactionFileClass();
-
                     transactionFile.appendTransactionToFile(transaction);
                 }
 
             } else if (role.equals("2")) {
-
                 System.out.print("Enter your name: ");
-
                 String employeeName = scanner.nextLine().trim();
-
                 Employee employee = new Employee(employeeName);
 
                 for (Transaction t : allTransactions) {
-
                     employee.receiveTransaction(t);
                 }
 
                 employee.viewAssignedTransactions();
 
             } else if (role.equals("3")) {
-
                 System.out.println("Thank you for using the system.");
-
                 break;
-
             } else {
-
                 System.out.println("Invalid option. Try again.");
             }
         }
