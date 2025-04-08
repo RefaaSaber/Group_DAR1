a694dd4e73146f2f22cf89eea97efc8df04806c8
RubaaAlghamdi <ralghamdi1255@stu.kau.edu.sa>
2025-04-08 20:38:34 +0300

    release 4 : (ruba ) refactory for TransactionFileClass class

--- src/cpit251_project/TransactionFileClass.java
+++ src/cpit251_project/TransactionFileClass.java
@@ -2,17 +2,35 @@ package cpit251_project;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class TransactionFileClass {
 
-    private static final String FILE_NAME = "transactions.txt";
+    private final String fileName;
+
+    public TransactionFileClass() {
+        this.fileName = "transactions.txt";
+    }
+
+    public TransactionFileClass(String customFileName) {
+        this.fileName = customFileName;
+    }
 
     public void appendTransactionToFile(Transaction transaction) {
-        try (FileWriter writer = new FileWriter(FILE_NAME, true)) {
-            writer.write(transaction.toString() + System.lineSeparator());
-            System.out.println("Transaction saved to file!");
+        try (FileWriter writer = new FileWriter(fileName, true)) {
+            writer.write(transaction.toString());
+            writer.write(System.lineSeparator());
+            writer.write("==========================================");
+            writer.write(System.lineSeparator());
+            System.out.println(" Transaction saved to file!");
         } catch (IOException e) {
-            System.out.println("Error saving transaction: " + e.getMessage());
+            System.out.println(" Error saving transaction: " + e.getMessage());
+        }
+    }
+
+    public void appendMultipleTransactions(List<Transaction> transactions) {
+        for (Transaction t : transactions) {
+            appendTransactionToFile(t);
         }
     }
 }
