825a94ac850ea7d5b96870d9d0165c187e6ceda4
RefaaSaber <rhusainsaber@stu.kau.edu.sa>
2025-04-10 16:29:34 +0300

    refactor to include a prompt for Transaction Content

--- src/cpit251_project/EmployeeAssignment.java
+++ src/cpit251_project/EmployeeAssignment.java
@@ -1,9 +1,10 @@
 package cpit251_project;
 
+import java.time.LocalDate;
 
 public class EmployeeAssignment {
-    
-     private String employeeName;
+
+    private String employeeName;
     private LocalDate deadline;
 
     public EmployeeAssignment(String employeeName, LocalDate deadline) {
@@ -11,10 +12,24 @@ public class EmployeeAssignment {
         this.deadline = deadline;
     }
 
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+    
+public void setEmployeeName(String employeeName) {
+    this.employeeName = employeeName;
+}
+
+
+public void setDeadline(LocalDate deadline) {
+    this.deadline = deadline;
+}
     @Override
     public String toString() {
-        return String.format("Employee: %s, Deadline: %s", employeeName, deadline);
-    }
+    return "Employee: " + employeeName + ", Deadline: " + deadline;
 }
-    
 }
